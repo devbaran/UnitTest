19a6f689395ff58e834cb40cec2e765d1729eed8
Baran Kurtbogan <barankurtbogan@gmail.com>
2023-05-28 01:44:30 +0300

    initial commit

---
+++ UnitTest/JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace JobApplicationLibrary.UnitTest
+{
+    public class ApplicationEvaluateUnitTest
+    {
+        /*
+         How to write test function name?
+        UnitOfWork_Condition_ExpectedResult or Condition_ExpectedResult or
+        UnitOfWork_ExpectedResult_Condition
+         */
+        [Test]
+        public void Application_WithUnderAge_TransferredToAutoRejected()
+        {
+            //Arrange
+            //Action
+            //Assert
+        }
+
+    }
+}
---
+++ UnitTest/JobApplicationLibrary/ApplicationEvaluator.cs
@@ -0,0 +1,28 @@
+﻿using JobApplicationLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobApplicationLibrary
+{
+    public class ApplicationEvaluator
+    {
+        private const int minAge = 18;
+        public ApplicationResult Evaluate(JobApplication form)
+        {
+            if (form.Applicant.Age < minAge)
+                return ApplicationResult.AutoReject;
+
+            return ApplicationResult.AutoAccept;
+        }
+    }
+    public enum ApplicationResult
+    {
+        AutoReject,
+        TransferToHR,
+        TransferToLead,
+        AutoAccept
+    }
+}
