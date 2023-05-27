db4bca2d4d03f93fb92ab2fe1f722e2231a3ac30
Baran Kurtbogan <barankurtbogan@gmail.com>
2023-05-28 01:55:41 +0300

    write first test

--- UnitTest/JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
+++ UnitTest/JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -1,3 +1,4 @@
+using JobApplicationLibrary.Models;
 using NUnit.Framework;
 
 namespace JobApplicationLibrary.UnitTest
@@ -8,13 +9,25 @@ namespace JobApplicationLibrary.UnitTest
          How to write test function name?
         UnitOfWork_Condition_ExpectedResult or Condition_ExpectedResult or
         UnitOfWork_ExpectedResult_Condition
+
+        - Every single test should be independent
          */
         [Test]
         public void Application_WithUnderAge_TransferredToAutoRejected()
         {
             //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var application = new JobApplication
+            {
+                Applicant = new Applicant { Age = 17 }
+            };
+
             //Action
+            var appResult = evaluator.Evaluate(application);
+
+
             //Assert
+            Assert.AreEqual(ApplicationResult.AutoReject, appResult);
         }
 
     }
