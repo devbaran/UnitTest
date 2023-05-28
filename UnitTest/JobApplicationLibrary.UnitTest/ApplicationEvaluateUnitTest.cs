d398e64e6eaa8dbabca2eba43b95f2be491291e7
Baran Kurtbogan <barankurtbogan@gmail.com>
2023-05-28 23:59:43 +0300

    add more test

--- UnitTest/JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
+++ UnitTest/JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -30,5 +30,43 @@ namespace JobApplicationLibrary.UnitTest
             Assert.AreEqual(ApplicationResult.AutoReject, appResult);
         }
 
+        [Test]
+        public void Application_WithNoTechStack_TransferredToAutoRejected()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var application = new JobApplication
+            {
+                Applicant = new Applicant { Age = 18 },
+                TechStackList = new System.Collections.Generic.List<string> { "" }
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(application);
+
+
+            //Assert
+            Assert.AreEqual(ApplicationResult.AutoReject, appResult);
+        }
+
+        [Test]
+        public void Application_WithTechStackOver75p_TransferredToAutoAccepted()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var application = new JobApplication
+            {
+                Applicant = new Applicant { Age = 18 },
+                TechStackList = new System.Collections.Generic.List<string> { "C#", "RabbitMQ", "Microservice", "Visual Studio" },
+                YearsOfExperience = 15
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(application);
+
+
+            //Assert
+            Assert.AreEqual(ApplicationResult.AutoAccept, appResult);
+        }
     }
 }
--- UnitTest/JobApplicationLibrary/ApplicationEvaluator.cs
+++ UnitTest/JobApplicationLibrary/ApplicationEvaluator.cs
@@ -10,12 +10,27 @@ namespace JobApplicationLibrary
     public class ApplicationEvaluator
     {
         private const int minAge = 18;
+        private const int autoAcceptedYearOfExperience = 15;
+        private List<string> techStackList = new List<string> { "C#", "RabbitMQ", "Microservice", "Visual Studio" };
         public ApplicationResult Evaluate(JobApplication form)
         {
             if (form.Applicant.Age < minAge)
                 return ApplicationResult.AutoReject;
 
+            var similartyRate = GetTechStackSimilarityRate(form.TechStackList);
+
+            if (similartyRate < 25)
+                return ApplicationResult.AutoReject;
+            if (similartyRate > 75 && form.YearsOfExperience >= autoAcceptedYearOfExperience)
+                return ApplicationResult.AutoAccept;
+
+
             return ApplicationResult.AutoAccept;
+        }
+        private int GetTechStackSimilarityRate(List<string> techStack)
+        {
+            var matchedCount = techStack.Where(a => techStackList.Contains(a, StringComparer.OrdinalIgnoreCase)).Count();
+            return (int)((double)matchedCount / techStackList.Count) * 100;
         }
     }
     public enum ApplicationResult
